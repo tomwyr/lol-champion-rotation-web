5364c7aeffabb7a313b0c0d482d45123109f701c
Tomasz Wyrowiński <tomwyr@hotmail.com>
2025-02-12 01:31:09 +0100

    Adjust padding

--- src/components/DataError.vue
+++ src/components/DataError.vue
@@ -2,7 +2,7 @@
   <div class="flex items-center justify-center min-h-screen">
     <div class="flex flex-col justify-center items-center">
       <IconWarning />
-      <h1 class="pt-2 text-gray-900 dark:text-gray-100">Failed to load data. Please try again.</h1>
+      <h1 class="pt-1 text-gray-900 dark:text-gray-100">Failed to load data. Please try again.</h1>
       <button
         @click="onRetry"
         class="mt-4 py-1.5 px-3 rounded text-sm mx-auto bg-gray-200 hover:bg-gray-300 text-gray-900 dark:bg-gray-800 dark:hover:bg-gray-700 dark:text-gray-100"
