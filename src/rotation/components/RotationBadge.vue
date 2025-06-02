bbac87c48bdcef2a131618840ec94c36a0c8b8da
Tomasz Wyrowiński <tomwyr@hotmail.com>
2025-06-02 23:59:15 +0200

    Use code completion for badge color classes

--- src/rotation/components/RotationBadge.vue
+++ src/rotation/components/RotationBadge.vue
@@ -8,35 +8,29 @@
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue'
-
 export type RotationBadgeVariant = 'current' | 'prediction'
 
 const { variant } = defineProps<{
   variant: RotationBadgeVariant
 }>()
 
-const label = computed(() => {
-  switch (variant) {
-    case 'current':
-      return 'Current'
-    case 'prediction':
-      return 'Prediction'
-    default:
-      variant satisfies never
-      return {}
-  }
-})
+let label: string
+let className: string
 
-const className = computed(() => {
-  switch (variant) {
-    case 'current':
-      return 'text-green-400 bg-green-400 border-green-400 dark:text-green-300 dark:bg-green-300 dark:border-green-300'
-    case 'prediction':
-      return 'text-amber-400 bg-amber-400 border-amber-400 dark:text-amber-300 dark:bg-amber-300 dark:border-amber-300'
-    default:
-      variant satisfies never
-      return {}
-  }
-})
+switch (variant) {
+  case 'current':
+    label = 'Current'
+    className =
+      'text-green-400 bg-green-400 border-green-400 dark:text-green-300 dark:bg-green-300 dark:border-green-300'
+    break
+
+  case 'prediction':
+    label = 'Prediction'
+    className =
+      'text-amber-400 bg-amber-400 border-amber-400 dark:text-amber-300 dark:bg-amber-300 dark:border-amber-300'
+    break
+
+  default:
+    variant satisfies never
+}
 </script>
