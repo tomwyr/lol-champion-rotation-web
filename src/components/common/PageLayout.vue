092a833b60745856e5ec55324c711414207f2d79
Tomasz Wyrowiński <tomwyr@hotmail.com>
2025-06-22 11:46:45 +0200

    Adjust header size param values

--- src/components/common/PageLayout.vue
+++ src/components/common/PageLayout.vue
@@ -2,8 +2,8 @@
   <header
     class="fixed z-50 inset-x-0 py-2 bg-inherit"
     :class="{
-      'h-[4rem] md:h-[4.5rem]': headerSize === 'large',
-      'h-[4rem]': headerSize === 'compact',
+      'h-[4rem] md:h-[4.5rem]': headerSize === 'loose',
+      'h-[4rem]': headerSize === 'tight',
     }"
   >
     <div class="lg:max-w-[768px] md:max-w-[600px] max-w-[480px] px-4 mx-auto h-full flex">
@@ -12,8 +12,8 @@
   </header>
   <main
     :class="{
-      'pt-[4rem] md:pt-[4.5rem]': headerSize === 'large',
-      'pt-[4rem]': headerSize === 'compact',
+      'pt-[4rem] md:pt-[4.5rem]': headerSize === 'loose',
+      'pt-[4rem]': headerSize === 'tight',
     }"
     class="lg:max-w-[768px] md:max-w-[600px] max-w-[480px] mx-auto px-4 flex flex-col h-screen"
   >
@@ -22,7 +22,7 @@
 </template>
 
 <script setup lang="ts">
-const { headerSize = 'large' } = defineProps<{
-  headerSize?: 'large' | 'compact'
+const { headerSize = 'loose' } = defineProps<{
+  headerSize?: 'loose' | 'tight'
 }>()
 </script>
