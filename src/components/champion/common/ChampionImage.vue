f9b1f7862a4515e558bcd68e88d59568900b8652
Tomasz Wyrowiński <tomwyr@hotmail.com>
2026-05-02 23:17:48 +0200

    Improve champion image rendering

--- src/components/champion/common/ChampionImage.vue
+++ src/components/champion/common/ChampionImage.vue
@@ -1,15 +1,22 @@
 <template>
-  <img loading="lazy" :src="imageUrl" @error="fallbackImageUrl" >
+  <img
+    class="bg-cover size-full"
+    :class="`bg-[url(${placeholder})]`"
+    :src="imageUrl"
+    loading="lazy"
+    @error="fallbackImageUrl"
+  />
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
 
 const props = defineProps<{ imageUrl: string }>()
-
 const imageUrl = ref(props.imageUrl)
 
+const placeholder = '/assets/champion-placeholder.png'
+
 function fallbackImageUrl() {
-  imageUrl.value = '/assets/champion-placeholder.png'
+  imageUrl.value = placeholder
 }
 </script>
--- src/components/rotation/common/ChampionRotationTile.vue
+++ src/components/rotation/common/ChampionRotationTile.vue
@@ -1,13 +1,14 @@
 <template>
-  <a :href="`/champions/${champion.id}`">
-    <div class="champion-tile relative cursor-pointer">
-      <ChampionImage class="rounded aspect-square" :image-url="champion.imageUrl" />
-      <div class="absolute bottom-0 inset-x-0 text-center">
-        <div
-          class="inline-block px-2 py-0.5 mb-2 text-sm rounded-full shadow-sm shadow-black text-gray-100 bg-gray-900/80"
-          v-text="champion.name"
-        />
-      </div>
+  <a
+    class="aspect-square champion-tile relative cursor-pointer"
+    :href="`/champions/${champion.id}`"
+  >
+    <ChampionImage class="rounded" :image-url="champion.imageUrl" />
+    <div class="absolute bottom-0 inset-x-0 text-center">
+      <div
+        class="inline-block px-2 py-0.5 mb-2 text-sm rounded-full shadow-sm shadow-black text-gray-100 bg-gray-900/80"
+        v-text="champion.name"
+      />
     </div>
   </a>
 </template>
