9c2608a973c6cf124cfdf72ad7121e5421ee5e0b
Tomasz Wyrowiński <tomwyr@hotmail.com>
2026-05-03 19:31:03 +0200

    Fix opening rotation details

--- src/components/rotation/common/ChampionRotationGrid.vue
+++ src/components/rotation/common/ChampionRotationGrid.vue
@@ -5,18 +5,19 @@
       '-m-1 p-1 rounded cursor-pointer transition-colors hover:[&:not(:has(.champion-tile:hover))]:bg-gray-200 dark:hover:[&:not(:has(.champion-tile:hover))]:bg-gray-800':
         rotation.detailsId,
     }"
+    :onclick="
+      rotation.detailsId ? `window.location.href = '/rotations/${rotation.detailsId}'` : undefined
+    "
   >
-    <a :href="rotation.detailsId ? `/rotations/${rotation.detailsId}` : undefined">
-      <div class="flex flex-row items-center">
-        <h2>{{ rotation.title }}</h2>
+    <div class="flex flex-row items-center">
+      <h2>{{ rotation.title }}</h2>
 
-        <RotationBadge v-if="rotation.badge" :variant="rotation.badge" />
-      </div>
+      <RotationBadge v-if="rotation.badge" :variant="rotation.badge" />
+    </div>
 
-      <h3 v-if="rotation.subtitle" class="font-light text-gray-500 dark:text-gray-400">
-        {{ rotation.subtitle }}
-      </h3>
-    </a>
+    <h3 v-if="rotation.subtitle" class="font-light text-gray-500 dark:text-gray-400">
+      {{ rotation.subtitle }}
+    </h3>
 
     <div
       v-if="rotation.champions.length > 0"
